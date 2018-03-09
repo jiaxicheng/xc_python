456ff6e39e8cb4de9902fc69c636929b40f34deb
jiaxicheng <xichengjia@gmail.com>
2018-03-09 13:57:55 -0500

    Added xauth package to handle the GUI interface when showing plot with matplotlib modified start up script and docker-compose.yml to accomondate this change.

--- service_xc_python.sh
+++ service_xc_python.sh
@@ -1,11 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
+# pre-defined Action OPTS and regex to match one of the OPTS
+OPTS=(build build-run up down start stop)
+regex=$(IFS=\|;echo "${OPTS[*]}")
+
+# usage
 USAGE="Usage:
 
-    $0 [-h|-d folder] <build|start|build-run|stop> [SERVICE..]
+    $0 [-h|-d folder] <$regex> [SERVICE..]
 "
 
+# shared folder to save working codes
 SHARED=/home/xicheng/my_code/python
+
+# process the command options
 while getopts "hd" opt; do
   case $opt in
     h)
@@ -13,20 +21,36 @@ while getopts "hd" opt; do
       ;;
     d)
       SHARED=$2
-      [[ -d "$SHARED" ]] || { echo "the folder $SHARED does not exist"; exit 1; }
+      [[ -d "$SHARED" ]] || { echo "the folder '$SHARED' does not exist"; exit 1; }
       ;;
   esac
 done
 shift $((OPTIND-1))
 
+# action must be one of the pre-defined OPTS
+[[ $1 =~ ^$regex$ ]] || { echo "$USAGE"; exit; }
+
+# SHARED folder supplied by -d argument or default to '/home/xicheng/my_code/python'
 export SHARED
+
+# set username, user_uid and user_gid the same as the owner of the SHARED folder
 export USER=$(stat -c "%U" "$SHARED")
 export USER_UID=$(stat -c "%u" "$SHARED")
 export USER_GID=$(stat -c "%g" "$SHARED")
 
+# retrieve one of the xauth cookie fromt he host server
+export XAUTH=$(xauth list | awk -v d=$DISPLAY '$1 ~ d{print "xauth add",d,$2,$3; exit}')
+
+# add xauth cookie into the file ~/.xauthrc <-- moved the flow to docker-compose.yml
+#echo "$XAUTH" >home/.xauthrc
+
+# wrapper for docker-compose command with required environments for the services
 case $1 in
-  start)
+  up)
     docker-compose up -d "${@:2}"
+    ;;
+  start)
+    docker-compose start "${@:2}"
     ;;
   build-run)
     docker-compose up -d --build "${@:2}"
@@ -34,8 +58,11 @@ case $1 in
   build)
     docker-compose build --force-rm "${@:2}"
     ;;
+  down)
+    docker-compose down --remove-orphans 
+    ;;
   stop)
-    docker-compose down --remove-orphans
+    docker-compose stop "${@:2}"
     ;;
   *)
     echo "$USAGE"
