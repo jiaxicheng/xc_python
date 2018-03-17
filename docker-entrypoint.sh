2812e36c0c29ebd111c49c7a7931f52b1ab5524f
jiaxicheng <xichengjia@gmail.com>
2018-03-17 00:37:02 -0400

    delete unused files

--- docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-user=${1:-xicheng}
-shared=${2:-/shared}
-
-jupyter notebook --ip=0.0.0.0 --port=9999 --no-browser --notebook-dir="$shared" >>/home/$user/logs/jupyter-notebook.log 2>&1
