43de203b5da89944201a67b2f4663a13afdb5dd6
jiaxicheng <xichengjia@gmail.com>
2018-03-17 00:22:14 -0400

    branch to master-minor adjustment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,6 @@
 user=${1:-xicheng}
 shared=${2:-/shared}
 
-mkdir ~/.jupyter ~/logs
+mkdir -p ~/.jupyter ~/logs
 
 jupyter notebook --ip=0.0.0.0 --port=9999 --no-browser --notebook-dir="$shared" >> ~/logs/jupyter-notebook.log 2>&1
