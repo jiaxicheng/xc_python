e8d5b0686b94ebc7c76796833f9f03762d5e6f97
lihao0129 <lihao0129@gmail.com>
2018-03-17 00:03:18 -0400

    branch to jupyter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-user=${1:-xicheng}
 shared=${2:-/shared}
 
-jupyter notebook --ip=0.0.0.0 --port=9999 --no-browser --notebook-dir="$shared" >>/home/$user/logs/jupyter-notebook.log 2>&1
+mkdir -p ~/.jupyter ~/logs
+
+jupyter notebook --ip=0.0.0.0 --port=9999 --no-browser --notebook-dir="$shared" >> ~/logs/jupyter-notebook.log 2>&1
