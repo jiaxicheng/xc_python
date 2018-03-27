cc52d4d618f56e1977d2616d3378cfa1c4e36b33
xichengjia <xichengjia@gmail.com>
2018-03-27 12:24:27 -0400

    remove unrelated files

--- docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-shared=${1:-/shared}
-
-mkdir -p ~/.jupyter ~/logs
-
-jupyter notebook --ip=0.0.0.0 --port=9999 --no-browser --notebook-dir="$shared" >> ~/logs/jupyter-notebook.log 2>&1
